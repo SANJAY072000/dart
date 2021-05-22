5b079949cd2f2e0b8e508a4a5184ceaa4511c791
Sanjay Singh Bisht <sanjaysinghbisht114@gmail.com>
2021-05-22 14:49:56 +0530

    Check type in dart

--- expert/apicall.dart
+++ expert/apicall.dart
@@ -2,8 +2,12 @@ import 'dart:io';
 import 'dart:convert';
 
 void main() {
-  HttpClient()
-      .getUrl(Uri.parse('https://randomuser.me/api/'))
-      .then((req) => req.close())
-      .then((res) => res.transform(Utf8Decoder()).listen((print)));
+  // HttpClient()
+  //     .getUrl(Uri.parse('https://randomuser.me/api/'))
+  //     .then((req) => req.close())
+  //     .then((res) => res.transform(Utf8Decoder()).listen((print)));
+  // var a1 = ['los', 'san', 'ny'];
+  // print(a1.firstWhere((c) => c.length < 10));
+  var m = {'a1': 5, 8: 'a2'};
+  print(m.runtimeType);
 }
